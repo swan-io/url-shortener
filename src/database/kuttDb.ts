d7167ce5361b7c350a73d41aaededc558ecffbf2
Mathieu Acthernoene <zoontek@gmail.com>
2024-04-24 16:48:05 +0200

    Extract kuttDb in another file

--- src/database/kuttDb.ts
+++ src/database/kuttDb.ts
@@ -1,3 +1,5 @@
+// TODO: remove this file once migration is done
+
 import { Kysely, PostgresDialect } from "kysely";
 import { Pool } from "pg";
 import { env } from "../utils/env";
@@ -5,25 +7,25 @@ import { DB } from "./generated/types";
 
 const dialect = new PostgresDialect({
   pool: new Pool({
-    connectionString: env.DATABASE_URL,
+    connectionString: env.KUTT_DATABASE_URL,
     min: 5,
     max: 20,
   }),
 });
 
 const kyselyGlobal = global as typeof global & {
-  db?: Kysely<DB>;
+  kuttDb?: Kysely<DB>;
 };
 
 // workaround to make kysely work well during "yarn dev"
 // @see https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
-export const db =
-  kyselyGlobal.db ??
+export const kuttDb =
+  kyselyGlobal.kuttDb ??
   new Kysely<DB>({
     dialect,
     log: env.NODE_ENV === "development" ? ["query", "error"] : ["error"],
   });
 
 if (env.NODE_ENV !== "production") {
-  kyselyGlobal.db = db;
+  kyselyGlobal.kuttDb = kuttDb;
 }
