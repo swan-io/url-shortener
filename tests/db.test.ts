5c2880f786b6693b250851f13f4ee90506b2a585
Mathieu Acthernoene <zoontek@gmail.com>
2024-04-25 10:46:26 +0200

    Clear table on afterEach

--- tests/db.test.ts
+++ tests/db.test.ts
@@ -3,26 +3,31 @@ import { sql } from "kysely";
 import fs from "node:fs/promises";
 import path from "node:path";
 import { PostgresMock } from "pgmock";
-import { beforeAll, expect, test } from "vitest";
+import { afterEach, beforeAll, expect, test } from "vitest";
 import { deleteExpiredLinks } from "../src/database/db";
 import { addToNow } from "../src/utils/time";
 
-const timeout = 60000;
+const timeout = 30000;
 
 beforeAll(async () => {
-  const [mock, schema] = await Promise.all([
-    PostgresMock.create(),
-    fs.readFile(path.join(__dirname, "schema.sql"), "utf-8"),
-  ]);
-
+  const mock = await PostgresMock.create();
   await mock.listen(25432);
+
   const { db } = await import("../src/database/db");
+  const schema = await fs.readFile(path.join(__dirname, "schema.sql"), "utf-8");
 
   // @ts-expect-error
   const migrate = sql(schema);
   await db.executeQuery(migrate.compile(db));
 
-  return () => mock.destroy();
+  return () => {
+    mock.destroy();
+  };
+}, timeout);
+
+afterEach(async () => {
+  const { db } = await import("../src/database/db");
+  await db.deleteFrom("links").executeTakeFirstOrThrow();
 }, timeout);
 
 test("correctly insert and get a link", { timeout }, async () => {
@@ -47,9 +52,6 @@ test("don't return an expired link", { timeout }, async () => {
 
   const link = await getLink(address);
   expect(link).toBeUndefined();
-
-  const { numDeletedRows } = await deleteExpiredLinks();
-  expect(numDeletedRows).toBe(BigInt(1));
 });
 
 test("delete expired links", { timeout }, async () => {
@@ -62,7 +64,7 @@ test("delete expired links", { timeout }, async () => {
     }),
     createLink({
       target: "https://swan.io",
-      expired_at: dayjs("01/01/2050").toISOString(),
+      expired_at: dayjs("01/01/2100").toISOString(),
     }),
   ]);
 
