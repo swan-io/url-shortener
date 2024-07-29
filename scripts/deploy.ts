55ca96180472d38cc1712e507d6172a2841a3cfb
Mathieu Acthernoene <zoontek@gmail.com>
2024-07-29 15:26:16 +0200

    Use valienv in script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,54 +1,59 @@
-import assert from "node:assert";
 import { execSync } from "node:child_process";
 import fs from "node:fs";
 import os from "node:os";
 import path from "node:path";
+import { string, validate } from "valienv";
 
 const tmp = os.tmpdir();
 const repoName = "deploy";
 
-assert(process.env.TAG);
-assert(process.env.DEPLOY_SWAN_TOKEN);
-assert(process.env.DEPLOY_SWAN_REPOSITORY);
-assert(process.env.DEPLOY_ENVIRONMENT);
-assert(process.env.DEPLOY_APP_NAME);
-assert(process.env.DEPLOY_GIT_USER);
-assert(process.env.DEPLOY_GIT_EMAIL);
+const env = validate({
+  env: process.env,
+  validators: {
+    TAG: string,
+    DEPLOY_SWAN_TOKEN: string,
+    DEPLOY_SWAN_REPOSITORY: string,
+    DEPLOY_ENVIRONMENT: string,
+    DEPLOY_APP_NAME: string,
+    DEPLOY_GIT_USER: string,
+    DEPLOY_GIT_EMAIL: string,
+  },
+});
 
-execSync(`git config --global user.name ${process.env.DEPLOY_GIT_USER}`);
-execSync(`git config --global user.email ${process.env.DEPLOY_GIT_EMAIL}`);
+execSync(`git config --global user.name ${env.DEPLOY_GIT_USER}`);
+execSync(`git config --global user.email ${env.DEPLOY_GIT_EMAIL}`);
 
 execSync(`rm -fr ${tmp}/${repoName}`);
 
 execSync(
-  `cd ${tmp} && git clone --single-branch --branch master https://projects:${process.env.DEPLOY_SWAN_TOKEN}@${process.env.DEPLOY_SWAN_REPOSITORY} ${repoName}`,
+  `cd ${tmp} && git clone --single-branch --branch master https://projects:${env.DEPLOY_SWAN_TOKEN}@${env.DEPLOY_SWAN_REPOSITORY} ${repoName}`,
 );
 
 const file = fs.readFileSync(
   path.join(
     tmp,
     repoName,
-    process.env.DEPLOY_ENVIRONMENT,
-    `${process.env.DEPLOY_APP_NAME}-values.yaml`,
+    env.DEPLOY_ENVIRONMENT,
+    `${env.DEPLOY_APP_NAME}-values.yaml`,
   ),
   "utf-8",
 );
 
-const updatedFile = file.replaceAll(/\btag: .+/g, `tag: ${process.env.TAG}`);
+const updatedFile = file.replaceAll(/\btag: .+/g, `tag: ${env.TAG}`);
 
 fs.writeFileSync(
   path.join(
     tmp,
     repoName,
-    process.env.DEPLOY_ENVIRONMENT,
-    `${process.env.DEPLOY_APP_NAME}-values.yaml`,
+    env.DEPLOY_ENVIRONMENT,
+    `${env.DEPLOY_APP_NAME}-values.yaml`,
   ),
   updatedFile,
   "utf-8",
 );
 
 execSync(
-  `cd ${tmp}/${repoName} && git commit --allow-empty -am "Update with tag: ${process.env.TAG}, image(s): ${process.env.DEPLOY_APP_NAME}"`,
+  `cd ${tmp}/${repoName} && git commit --allow-empty -am "Update with tag: ${env.TAG}, image(s): ${env.DEPLOY_APP_NAME}"`,
 );
 
 execSync(
