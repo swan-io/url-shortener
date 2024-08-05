bf8022413013b6d387c80753552343364e2e8b53
Mathieu Acthernoene <zoontek@gmail.com>
2024-08-05 12:47:58 +0200

    Improve auth plugin

--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -3,11 +3,14 @@ import { env } from "../utils/env";
 
 export const auth = fp(async (fastify) => {
   fastify.addHook("onRequest", async (request, reply) => {
+    const { url } = request;
+
     // TODO: remove /api/v2/health after migration
     if (
-      request.url.startsWith("/api/") &&
-      request.url !== "/api/health" &&
-      request.url !== "/api/v2/health"
+      url === "/api" ||
+      (url.startsWith("/api/") &&
+        url !== "/api/health" &&
+        url !== "/api/v2/health")
     ) {
       if (request.headers["x-api-key"] !== env.API_KEY) {
         return reply.unauthorized();
