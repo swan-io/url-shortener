825c0219bdba3869cff164af9572758b055e6667
Mathieu Acthernoene <zoontek@gmail.com>
2024-08-09 12:04:05 +0200

    Add a metrics endpoint

--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -1,11 +1,13 @@
 import fp from "fastify-plugin";
 import { env } from "../utils/env";
 
+const whitelist = new Set(["/api/health", "/api/metrics"]);
+
 export const auth = fp(async (fastify) => {
   fastify.addHook("onRequest", async (request, reply) => {
     const { url } = request;
 
-    if (url === "/api" || (url.startsWith("/api/") && url !== "/api/health")) {
+    if (url === "/api" || (url.startsWith("/api/") && !whitelist.has(url))) {
       if (request.headers["x-api-key"] !== env.API_KEY) {
         return reply.unauthorized();
       }
